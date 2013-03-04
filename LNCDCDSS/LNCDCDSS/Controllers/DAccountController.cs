86e8e637d6859bd32fd2b159967dea14258d51d8
Jane <fguanjun1004@gmail.com>
2013-03-04 10:42:20 +0800

    登录的demo实现，需要转成AJAX

--- LNCDCDSS/LNCDCDSS/Controllers/DAccountController.cs
+++ LNCDCDSS/LNCDCDSS/Controllers/DAccountController.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using LNCDCDSS.Models;
 namespace LNCDCDSS.Controllers
 {
     public class DAccountController : Controller
     {
+
+        LNCDDataModelContainer DataContainer = new LNCDDataModelContainer();
         //
         // GET: /DAccount/
-
+       
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(DoctorAccount dacount)
+        {
+            var students = from s in DataContainer.DoctorAccountSet.ToList() select s;
+            var student = students.Where(s => s.UserName==dacount.UserName ).FirstOrDefault();
+          if (student!=null)
+          {
+              return Redirect("/EnterPatInfor/Index");
+          }
+          else
+            {
 
+                return View(dacount);
+            }
+          
+
+           
+        }
     }
 }
---
+++ LNCDCDSS/LNCDCDSS/Models/PatOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LNCDCDSS.Models
+{
+    public class PatOperation
+    {
+        LNCDDataModelContainer context = new LNCDDataModelContainer();
+        public IEnumerable<PatBasicInfor> GetPats()
+        {
+            return context.PatBasicInforSet.ToList();
+        }
+        public void InsertPat(PatBasicInfor pat)
+        {
+            context.PatBasicInforSet.Add(pat);
+        }
+    }
+}
