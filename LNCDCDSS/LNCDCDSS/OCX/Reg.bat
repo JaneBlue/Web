bbdcaf9ef2769149064f0c0609b22d3dba8a87d2
daowangli@gmail.com <daowangli@gmail.com>
2013-06-03 10:32:07 +0800

    add ocx

---
+++ LNCDCDSS/LNCDCDSS/OCX/Reg.bat
@@ -0,0 +1,14 @@
+regsvr32 .\Rep2Utility.dll      -s
+regsvr32 .\Rep2GDIPlusEx.dll    -s 
+regsvr32 .\Rep2ActiveScript.dll -s
+regsvr32 .\Rep2Common.dll       -s
+regsvr32 .\Rep2Control.dll      -s
+regsvr32 .\Rep2DataSource.dll   -s
+regsvr32 .\Rep2EditPlus.dll     -s
+regsvr32 .\Rep2ImageGrid.dll    -s
+regsvr32 .\Rep2Page.dll         -s
+regsvr32 .\Rep2Rule.dll         -s
+regsvr32 .\Rep2Sketch.dll       -s
+regsvr32 .\Rep2Parser.dll       -s
+regsvr32 .\Rep2SVGParser.dll    -s
+regsvr32 .\Reporter2.ocx
